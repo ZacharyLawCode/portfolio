cd28e036cb9a972f0563b7a7e08b06408eab0042
ZacharyLawCode <zacharylaw2002@gmail.com>
2025-02-16 02:33:52 -0600

    app restructure

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+// src/App.tsx
+import React from 'react';
+
+const App: React.FC = () => {
+  return <div>Hello World</div>;
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,9 @@ import './index.css'; // This will include your Tailwind CSS setup
 const rootElement = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 
+// Simply Render the App.jsx config file.
 root.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> 
+    <App /> 
   </React.StrictMode>
-);
+); 
